74e481a489079a8a08997e5bdf638ef326565167
Lauri Korts-Pärn <lauri@korc.jp>
2012-04-08 09:12:32 +0900

    rewrite gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,11 @@
 import os,sys,imp
 import gtk
 import packetlib
+from packetlib import BasePacketClass, DataMismatchError, BaseAttrClass,\
+	ArrayAttr
+
+def shrtn(s,maxlen=20):
+	return "%r%s"%(s[:20],"" if len(s)<20 else "...")
 
 class GtkBuilderUI(object):
 	def __init__(self,filename,cbobj=None):
@@ -32,116 +37,74 @@ class DummyPacket(packetlib.BasePacketClass):
 	_fields_=packetlib.AttrList(("data",packetlib.StringSZ))
 
 class PacketTreeModel(gtk.GenericTreeModel):
-	packet=None
-	def on_get_flags(self): return 0
-	def on_get_n_columns(self): return 3
-	def on_get_column_type(self, index): return str
-	#@_fmon
+	cols=["name","repr","type"]
+	cols_map=dict(map(reversed,enumerate(cols)))
+	# mandatory
+	def path_to_obj(self, path):
+		cur=None
+		for x in path:
+			if cur is None and x==0: cur=self.packet
+			else: cur=cur[x]
+		return cur
+	def on_get_flags(self):
+		return 0
+	def on_get_n_columns(self):
+		return len(self.cols)
+	def on_get_column_type(self, index):
+		return str
 	def on_get_iter(self, path):
-		if self.packet is None: return None
-		if path[0]==0:
-			ret=()
-			for p in path[1:]:
-				pkt=self._ref_to_obj(ret)
-				if isinstance(pkt,packetlib.ArrayAttr): ret=ret+(p,)
-				else: ret=ret+(pkt._fields_.names[p],)
-			return ret
-		raise NotImplementedError,"on_get_iter not implemented for path=%r"%(path,)
+		return path
 	def on_get_path(self, rowref):
-		ret=(0,)
-		obj=self.packet
-		for idx,name in enumerate(rowref):
-			if type(name)==int: n=name
-			else: n=obj._fields_.name_index[name]
-			ret=ret+(n,)
-			if idx<len(rowref)-1: obj=getattr(obj,name)
-		print 'on_get_path: %r -> %r (%r)'%(rowref,ret,obj)
-		return ret
-	#@_fmon
+		return rowref
 	def on_get_value(self, rowref, column):
-		if self.packet is None: return None
-		if column is 0:
-			if rowref==(): return self.packet.__class__.__name__
-			return rowref[-1]
-		elif column is 1:
-			obj=self._ref_to_obj(rowref)
-			r=obj._repr()
-			if isinstance(obj,packetlib.StringSZ) and len(r)>32: r='%s..'%(r[:32])
-			return r
-		elif column is 2:
-			cls=type(self._ref_to_obj(rowref))
-			return '%s.%s'%(cls.__module__,cls.__name__)
-		raise NotImplementedError,"on_get_value not implemented for rowref=%r, column=%r"%(rowref,column)
-	#@_fmon
+		colname=self.cols[column]
+		parent=self.path_to_obj(rowref[:-1])
+		idx=rowref[-1]
+		if parent is None and idx==0:
+			if colname=="name": return "<Packet>"
+			elif colname=="repr": return self.packet._repr()
+			elif colname=="type": return self.packet.__class__.__name__
+		if colname=="name":
+			if isinstance(parent,ArrayAttr): return "[%d]"%idx
+			field=parent._fields_[idx]
+			try: return field.name
+			except AttributeError: return "const[%d]"%idx
+		elif colname=="repr":
+			obj=parent[idx]
+			if isinstance(obj,str): return "[%d]=%s"%(idx,shrtn(obj))
+			return obj._repr()
+		elif colname=="type":
+			obj=parent[idx]
+			return type(obj).__name__
+		raise NotImplementedError("on_get_value", (rowref,colname))
 	def on_iter_next(self, rowref):
-		if rowref==(): return None
-		obj,name=self._ref_to_obj(rowref[:-1]),rowref[-1]
-		if type(name)==int:
-			if len(obj.dlist)>name+1: return rowref[:-1]+(name+1,)
-			else: return None
-		idx=obj._fields_.name_index[name]
-		try: return rowref[:-1]+(obj._fields_.names[idx+1],)
-		except IndexError: return None
-		raise NotImplementedError,"on_iter_next not implemented for rowref=%r"%(rowref,)
-	#@_fmon
+		if rowref==(0,): return None
+		idx=rowref[-1]
+		if self.on_iter_n_children(rowref[:-1])>idx+1:
+			return rowref[:-1]+(idx+1,)
+		else: return None
 	def on_iter_children(self, parent):
-		obj=self._ref_to_obj(parent)
-		if isinstance(obj,packetlib.ArrayAttr):
-			return parent+(0,)
-		else: return parent+(obj._fields_.names[0],)
-	#@_fmon
+		return parent+(0,)
 	def on_iter_has_child(self, rowref):
-		obj=self._ref_to_obj(rowref)
-		if obj is None: return False
-		elif isinstance(obj,packetlib.BasePacketClass): return True
-		elif isinstance(obj,packetlib.ArrayAttr): return len(obj.dlist)>0
-		else: return False
-	#@_fmon
+		obj=self.path_to_obj(rowref)
+		return isinstance(obj,(BasePacketClass,ArrayAttr))
 	def on_iter_n_children(self, rowref):
-		if rowref is None:
-			if self.packetlib is None: return 0
-			else: return 1
-		obj=self._ref_to_obj(rowref)
-		if isinstance(obj,packetlib.ArrayAttr): return len(obj.dlist)
-		return len(obj._fields_.names)
-		raise NotImplementedError,"on_iter_n_children not implemented for rowref=%r"%(rowref,)
-	#@_fmon
+		obj=self.path_to_obj(rowref)
+		if isinstance(obj, ArrayAttr):
+			try: return obj.count
+			except AttributeError:
+				return sum(map(lambda x:1,obj))
+		return len(obj._fields_.flow)
 	def on_iter_nth_child(self, parent, n):
-		if parent is None and n==0 and self.packet is not None: return ()
-		obj=self._ref_to_obj(parent)
-		if isinstance(obj,packetlib.ArrayAttr) and n<len(obj.dlist):
-			return parent+(n,)
-		try: return parent+(obj._fields_.names[n],)
-		except IndexError: pass
-		raise NotImplementedError,"on_iter_nth_child not implemented for parent=%r, n=%r"%(parent,n)
-	#@_fmon
+		if parent is None and n==0: return (0,)
+		return parent+(n,)
 	def on_iter_parent(self, child):
 		return child[:-1]
-	def _ref_to_obj(self,ref):
-		ret=self.packet
-		for attr in ref:
-			if type(attr)==int: ret=ret[attr]
-			else:
-				try: ret=getattr(ret,attr)
-				except AttributeError:
-					print "Error getting %r"%(ref,)
-					raise
-		return ret
-	def set_packet(self,packet):
-		if self.packet is None:
-			if packet is None: change=0
-			else: change=1
-		else:
-			if packet is None: change=2
-			else: change=3
+	# end of mandatory methods
+	def __init__(self, packet):
 		self.packet=packet
-		self.invalidate_iters()
-		topiter=self.get_iter_root()
-		if change is 1: self.row_inserted((0,),topiter)
-		elif change is 2: self.row_deleted((0,))
-		elif change is 3: self.row_changed((0,),topiter)
-		if change in (1,3):
-			self.row_has_child_toggled((0,),topiter)
+		super(PacketTreeModel, self).__init__()
+
 
 class GtkUI(object):
 	def __getattr__(self,key):
@@ -149,8 +112,6 @@ class GtkUI(object):
 		raise AttributeError,"No %r attribute"%(key)
 	def __init__(self):
 		self.ui=GtkBuilderUI(os.path.join(os.path.dirname(__file__),'gui.ui'),self)
-		self.pktstore=PacketTreeModel()
-		self.ui.pktree.set_model(self.pktstore)
 		self.ui.pktree.insert_column_with_attributes(-1,'Pkt',gtk.CellRendererText(),text=0)
 		self.ui.pktree.insert_column_with_attributes(-1,'Value',gtk.CellRendererText(),text=1)
 		self.ui.pktree.insert_column_with_attributes(-1,'Type',gtk.CellRendererText(),text=2)
@@ -165,10 +126,11 @@ class GtkUI(object):
 		self.reload()
 	def reload(self):
 		self.ui.offsetentry.set_text(str(self.data_offset))
+		self.ui.pktree.set_model(None)
 		if self.pclass is not None and self.data is not None:
-			self.pktstore.set_packet(self.pclass(self.data,self.data_offset))
-		else:
-			self.pktstore.set_packet(None)
+			try: packet=self.pclass(self.data,self.data_offset)
+			except DataMismatchError: pass
+			else: self.ui.pktree.set_model(PacketTreeModel(packet))
 	def run(self):
 		self.ui.offsetentry.set_text(str(self.data_offset))
 		if self.pclass is not None and self.pmod is not None:
@@ -208,4 +170,6 @@ if __name__=='__main__':
 		ui.load_pfile(pfile)
 		ui.set_pclass(pclass)
 	if len(sys.argv)>2: ui.set_file(sys.argv[2])
+	import pdb
+	sys.excepthook=lambda exctype, value, traceback: pdb.post_mortem(traceback if traceback else sys.exc_info()[2])
 	ui.run()
