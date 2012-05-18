f863fd29c1af7de45f519db958f848b0d436a933
Lauri Korts-Pärn <lauri@korc.jp>
2012-05-18 09:33:56 +0900

    add pattern finder to protlib

--- structx/protlib.py
+++ structx/protlib.py
@@ -2,6 +2,7 @@
 
 import socket,select
 from packetlib import DynamicAttrClass
+import sys
 
 class NetworkError(Exception): pass
 class TimeoutError(NetworkError): pass
@@ -44,3 +45,51 @@ class TcpClient(DynamicAttrClass):
 		try: sock.send('')
 		except socket.error: return False
 		return True
+
+class PatFinder(object):
+	read_size=256*1024
+	class Match(object):
+		__slots__=["_match", "name", "check"]
+		def __init__(self, match, name, check):
+			self._match=match
+			self.name=name
+			self.check=check
+		def __getattr__(self, key):
+			return getattr(self._match, key)
+		def __getitem__(self, key):
+			return self._match.group(key)
+	def __init__(self, *checks, **attr):
+		self.checks=checks
+		for k,v in attr.iteritems(): setattr(self, k, v)
+	def data_skipped(self, data):
+		print >>sys.stderr,"Skipped data: %r"%(data,)
+	def search(self, data):
+			ret_st=ret=ret_check=ret_idx=None
+			for idx,check in enumerate(self.checks):
+				if isinstance(check, tuple): idx,check=check
+				match=check.search(data)
+				if match:
+					st=match.start()
+					if ret is None or st<ret_st:
+						ret,ret_check,ret_st,ret_idx=match,check,st,idx
+			if ret_st:
+				self.data_skipped(data[:ret_st])
+			if ret is not None:
+				return self.Match(ret,check=ret_check,name=ret_idx)
+	def read_func(self, data_src):
+		return data_src.read(self.read_size)
+	def finditer(self, data_src, read_func=None):
+		remaining_buffer=""
+		if read_func is None: read_func=self.read_func
+		while True:
+			match=self.search(remaining_buffer)
+			if match is not None:
+				remaining_buffer=remaining_buffer[match.end():]
+				yield match
+			if match is None or not remaining_buffer:
+				data=read_func(data_src)
+				if data=="":
+					if remaining_buffer:
+						self.data_skipped(remaining_buffer)
+					return
+				remaining_buffer+=data
